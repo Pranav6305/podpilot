6ce3688654f7ccd1335a23098679a1f269e0d2e3
pranav <pranav@BLR-DGR1-L1.corp.ad.wrs.com>
2026-06-02 18:38:36 +0000

    Create API requests

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,21 +1,25 @@
 from fastapi import FastAPI
 
-from app.database.database import engine
 from app.database.base import Base
+from app.database.database import engine
 
+# Import model so SQLAlchemy registers the table
 from app.models.deployment import Deployment
-from app.routes.deployments import router as deployment_router
 
-# print(Base.metadata.tables.keys())
+from app.routes.deployments import router as deployment_router
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="PodPilot API", version="1.0.0")
+app = FastAPI(
+    title="PodPilot API",
+    version="1.0.0"
+)
 
 app.include_router(deployment_router)
+
 
 @app.get("/")
 def root():
     return {
-        "message": "Welcome to PodPilot. Its good to see you. Hope you had a good experience"
+        "message": "Welcome to PodPilot. It's good to see you. Hope you had a good experience"
     }
--- backend/app/schemas/deployment.py
+++ backend/app/schemas/deployment.py
@@ -1,7 +1,22 @@
 from pydantic import BaseModel
-# incoming API Request
+from datetime import datetime
+
+
 class DeploymentCreate(BaseModel):
     app_name: str
     image_name: str
     replicas: int
     container_port: int
+
+
+class DeploymentResponse(BaseModel):
+    id: int
+    app_name: str
+    image_name: str
+    replicas: int
+    container_port: int
+    status: str
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
